9b8520a0b4f718d567317fb35dbd70f60e48841c
MrMelick <Antoi@DESKTOP-U6ICED4>
2023-06-30 21:04:47 -0400

    affichage des rikihsis

--- WinFormsApp2/frmManagePool.cs
+++ WinFormsApp2/frmManagePool.cs
@@ -16,6 +16,7 @@ namespace SumoPoolUI
             listScore.View = View.Details; // Display items in a grid
             listScore.Columns.Add("Name", 120);
             listScore.Columns.Add("Score", 50);
+            listScore.Columns.Add("Rikishi", 550);
         }
 
         private async void btnCalculerScore_Click(object sender, EventArgs e)
@@ -28,7 +29,7 @@ namespace SumoPoolUI
             listScore.Items.Clear();
             foreach (var particiant in orderedResults)
             {
-                AddItem(particiant.Name, particiant.Score);
+                AddItem(particiant.Name, particiant.Score, particiant.Rikishis);
             }
             listScore.Refresh();
         }
@@ -45,17 +46,20 @@ namespace SumoPoolUI
                     listScore.Items.Clear();
                     foreach (var particiant in Pool.Participants)
                     {
-                        AddItem(particiant.Name, particiant.Score);
+                        AddItem(particiant.Name, particiant.Score, particiant.Rikishis);
                     }
                     listScore.Refresh();
                 }
             }
         }
 
-        private void AddItem(string name, int score)
+        private void AddItem(string name, int score, List<Rikishi> rikishis)
         {
             var item = new ListViewItem(name);
             item.SubItems.Add(score.ToString());
+            var listofRikishiNames = rikishis.Select(r => r.Name);
+            listofRikishiNames = listofRikishiNames.OrderBy(r => r);
+            item.SubItems.Add(string.Join(", ", listofRikishiNames));
             listScore.Items.Add(item);
         }
     }
