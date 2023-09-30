d7e23334aaacf887e0737cad8393a4f0af415892
MrMelick <Antoi@DESKTOP-U6ICED4>
2023-09-30 01:11:42 -0400

    changement, utilisation d'un csv au lieu d'un fichier json

---
+++ SumoPoolManager/Models/CsvRecords.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace SumoPoolManager.Models
+{
+    public class CsvRecords
+    {
+        [Index(0)]
+        public string Timestamp { get; set; }
+        [Index(1)]
+        public string Username { get; set; }
+        [Index(2)]
+        public string PseudoTwitch { get; set; }
+        [Index(3)]
+        public string YokozunaOzeki { get; set; }
+        [Index(4)]
+        public string Sekiwake { get; set; }
+        [Index(5)]
+        public string KomusubiAndMaegashira1 { get; set; }
+        [Index(6)]
+        public string Maegashira2To4 { get; set; }
+        [Index(7)]
+        public string Maegashira5To7 { get; set; }
+        [Index(8)]
+        public string Maegashira8To11 { get; set; }
+        [Index(9)]
+        public string Maegashira12To17 { get; set; }
+        [Index(10)]
+        public string RikishiReplacement1 { get; set; }
+        [Index(11)]
+        public string RikishiReplacement2 { get; set; }
+    }
+}
--- WinFormsApp2/frmManagePool.cs
+++ WinFormsApp2/frmManagePool.cs
@@ -1,6 +1,8 @@
 ﻿using SumoPoolManager.Models;
 using System.Text.Json;
 using SumoPoolManager.Services;
+using CsvHelper;
+using System.Globalization;
 
 namespace SumoPoolUI
 {
@@ -21,6 +23,12 @@ namespace SumoPoolUI
 
         private async void btnCalculerScore_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtPool.Text))
+            {
+                MessageBox.Show("Veuilez entrer un identifiant de pool valide soit annéjour genre 202309 pour le pool de sepemtre 2023", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Pool.TimestampId = txtPool.Text;
             var resultatsNotOrdrered = await ScoreCalculator.CalculateScoreForPoolUntilSelectedDay(
                 Pool.Participants,
                 Pool.TimestampId,
@@ -33,26 +41,6 @@ namespace SumoPoolUI
             }
             listScore.Refresh();
         }
-
-        private void ouvrirUnPoolToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (fileDialogPool.ShowDialog() == DialogResult.OK)
-            {
-                using var poolStream = File.OpenRead(fileDialogPool.FileName);
-                Pool = JsonSerializer.Deserialize<Pool>(poolStream);
-                if (Pool != null)
-                {
-                    lblFileName.Text = Pool.TimestampId;
-                    listScore.Items.Clear();
-                    foreach (var particiant in Pool.Participants)
-                    {
-                        AddItem(particiant.Name, particiant.Score, particiant.Rikishis);
-                    }
-                    listScore.Refresh();
-                }
-            }
-        }
-
         private void AddItem(string name, int score, List<Rikishi> rikishis)
         {
             var item = new ListViewItem(name);
@@ -61,6 +49,38 @@ namespace SumoPoolUI
             listofRikishiNames = listofRikishiNames.OrderBy(r => r);
             item.SubItems.Add(string.Join(", ", listofRikishiNames));
             listScore.Items.Add(item);
+        }
+
+        private void ouvrirUnPoolCsvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileDialogPool.ShowDialog() == DialogResult.OK)
+            {
+                using var poolStream = new StreamReader(fileDialogPool.FileName);
+                using var csv = new CsvReader(poolStream, CultureInfo.InvariantCulture);
+                var records = csv.GetRecords<CsvRecords>().ToList();
+                Pool = new();
+                listScore.Items.Clear();
+                foreach (var record in records)
+                {
+                    var participant = new Participant
+                    {
+                        Name = record.PseudoTwitch,
+                        Rikishis = new List<Rikishi>
+                        {
+                            new Rikishi { Name = record.YokozunaOzeki},
+                            new Rikishi { Name = record.Sekiwake},
+                            new Rikishi { Name = record.KomusubiAndMaegashira1},
+                            new Rikishi { Name = record.Maegashira2To4},
+                            new Rikishi { Name = record.Maegashira5To7},
+                            new Rikishi { Name = record.Maegashira8To11 },
+                            new Rikishi { Name= record.Maegashira12To17 }
+                        }
+                    };
+                    Pool.Participants.Add(participant);
+                    AddItem(participant.Name, participant.Score, participant.Rikishis);
+                    listScore.Refresh();
+                }
+            }
         }
     }
 }
